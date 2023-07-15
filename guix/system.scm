106e102daec4807088a289594c7a5ec7c54ed5c4
Christina O'Donnell <cdo@mutix.org>
2023-07-15 15:55:12 +0100

    guix/system.scm: Enable nvidia options and crashkernel

--- guix/system.scm
+++ guix/system.scm
@@ -74,9 +74,10 @@
 
 (operating-system
   (kernel linux)
-  ;; (kernel-arguments (list "module_blacklist=nouveau"
-                          ;; "nvidia_drm.modeset=1"))
-  ;; (kernel-arguments (list "crashkernel=256M"))
+  (kernel-arguments
+   (list "module_blacklist=nouveau"
+         "nvidia_drm.modeset=1"
+         "crashkernel=256"))
   (firmware (list linux-firmware))
   ;; Should include radeon-firmware
   (initrd microcode-initrd)
