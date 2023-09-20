0e05ded62ca762719c045fb1a8913c9cec2e11d5
Christina O'Donnell <cdo@mutix.org>
2023-09-20 17:33:49 +0100

    Add machines.scm for build offloading

---
+++ machines.scm
@@ -0,0 +1,5 @@
+(list (build-machine
+       (name "peter")
+       (systems (list "x86_64-linux" "i686-linux"))
+       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6tFSA6QQDicX8VA+Y0ek9t3+ZRcB2NrNUaMlD9CimD root@(none)")
+       (user "cdo")))
