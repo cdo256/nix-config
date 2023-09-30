f11dd7f37decc607d09d7950cdf5c82bf9ffb226
Christina O'Donnell <cdo@mutix.org>
2023-09-30 11:25:41 +0100

    Encrypt isaac's home directory

--- isaac.scm
+++ isaac.scm
@@ -31,12 +31,16 @@
                         (list "root=UUID=ec5539cc-5eb5-4a0a-a919-1de9fec3f248"))
                        (device "1b40ff94-038e-994f-a7d2-f14e1c01dde4"))))
                (keyboard-layout keyboard-layout)))
+  (mapped-devices
+    (list (mapped-device
+	    (source (uuid "a7378653-dea4-48dc-a8c8-b65e31745dd3"))
+	    (target "home")
+	    (type luks-device-mapping))))
   (file-systems (cons* (file-system
+			(device (file-system-label "home"))
                         (mount-point "/home")
-                        (device (uuid
-                                 "f99bd3f2-545c-4747-bb63-721bc7282e98"
-                                 'ext4))
-                        (type "ext4"))
+                        (type "ext4")
+			(dependencies mapped-devices))
                        (file-system
                         (mount-point "/")
                         (device (uuid
@@ -51,3 +55,4 @@
   (sudoers-file (local-file "/home/cdo/config/sudoers"))
   (hosts-file (local-file "/home/cdo/config/hosts")))
 
+;;/dev/sda5: UUID="a7378653-dea4-48dc-a8c8-b65e31745dd3" TYPE="crypto_LUKS" PARTUUID="5cb2ecde-199f-284b-84d1-dcbba217ad6b"
