098ca7a2f3ba859640a2cb236ee1a872ab8163a1
Christina O'Donnell <cdo@mutix.org>
2023-10-30 18:24:44 +0000

    guix: switch guix channel to master

--- guix/channels.scm
+++ guix/channels.scm
@@ -2,7 +2,7 @@
 
 (list (channel-with-substitutes-available (channel
 	(name 'guix)
-	(branch 'staging)
+	; (branch 'staging)
 	(url "https://git.savannah.gnu.org/git/guix.git")
 	(introduction (make-channel-introduction
 		"9a2e8664ecd0b7fe3371cb268506e68037b3263d"
