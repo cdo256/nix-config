cf5506dcff137787d06ae521978282df4375a488
Christina O'Donnell <cdo@mutix.org>
2024-04-05 23:32:55 +0100

    Get lvim loading working

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,21 +2,19 @@ local base_dir = vim.env.LUNARVIM_BASE_DIR
   -- or "/home/cdo/src/nvim-lunar"
   or "/home/cdo/.guix-home/profile/share/nvim";
 
-vim.opt.rtp:prepend("/home/cdo/.guix-home/profile/share/nvim/")
-vim.opt.rtp:prepend("/home/cdo/.guix-home/profile/share/nvim/site")
-vim.opt.rtp:prepend("/home/cdo/.guix-home/profile/share/nvim/site/pack")
-vim.opt.rtp:prepend("/home/cdo/.guix-home/profile/share/nvim/site/pack/guix")
-vim.opt.rtp:prepend("/home/cdo/.guix-home/profile/share/nvim/site/pack/guix/start")
-vim.opt.rtp:prepend(base_dir)
-if not vim.tbl_contains(vim.opt.rtp:get(), base_dir) then
-end
+package.path = table.concat({
+	package.path,
+	'/home/cdo/.guix-home/profile/share/nvim/?.lua',
+	'/home/cdo/.guix-home/profile/share/nvim/?/init.lua',
+	'/home/cdo/.guix-home/profile/share/nvim/site/pack/guix/start/lvim/lua/?.lua',
+	'/home/cdo/.guix-home/profile/share/nvim/site/pack/guix/start/lvim/lua/?/init.lua',
+}, ';')
 
 require("lvim.bootstrap"):init(base_dir)
 
 local Log = require "lvim.core.log"
 Log:debug('Search path:')
 Log:debug(package.path)
-package.path
 
 require("lvim.config"):load()
  
