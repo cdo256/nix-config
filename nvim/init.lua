c3fbe60f0a688cdd6374c7141082cbb8976b04be
Christina O'Donnell <cdo@mutix.org>
2024-04-05 19:09:25 +0100

    nvim: Add ../site/pack/guix/start to rtp

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ local base_dir = vim.env.LUNARVIM_BASE_DIR
   or "/home/cdo/.guix-home/profile/share/nvim/site"
 
 if not vim.tbl_contains(vim.opt.rtp:get(), base_dir) then
+  vim.opt.rtp:prepend("/home/cdo/.guix-home/profile/share/nvim/site/pack/guix/start")
   vim.opt.rtp:prepend(base_dir)
 end
 
