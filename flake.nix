bf39865a2b07a75fc23de3a6506a3393556884bd
Christina O'Donnell <cdo@mutix.org>
2024-10-17 20:04:51 +0100

    Rename nixos configuration.

--- flake.nix
+++ flake.nix
@@ -11,11 +11,11 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.halley = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
-        inputs.home-manager.nixosModules.default
+        #inputs.home-manager.nixosModules.default
       ];
     };
   };
