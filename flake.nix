4aac1a9ff3bfd992ca48b865aac50a83f45a408a
Christina O'Donnell <cdo@mutix.org>
2024-11-23 21:58:11 +0000

    Broken useless code.

--- flake.nix
+++ flake.nix
@@ -8,27 +8,43 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs: 
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }@inputs: 
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.gnumake ];
-      };
+      #devShells.x86_64-linux.default = pkgs.mkShell {
+      #  nativeBuildInputs = [ pkgs.gnumake ];
+      #};
       nixosConfigurations = {
         halley = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           specialArgs = {
             inherit inputs;
             devices = import hosts/devices.nix;
           };
           modules = [
+            
             ./hosts/halley/hardware-configuration.nix
             ./hosts/halley/configuration.nix
-            inputs.home-manager.nixosModules.default
+            {
+              #home-manager.useGlobalPkgs = true;
+              #home-manager.userUserPackakges = true;
+              home-manager = {
+                users.cdo = import ./home/client.nix;
+              };
+            }
+            home-manager.nixosModules.default
           ];
         };
         peter = nixpkgs.lib.nixosSystem {
