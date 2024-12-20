a80f4f862f5edf333954b9dfc051ac2dfb74ca27
Christina O'Donnell <cdo@mutix.org>
2024-12-20 13:57:58 +0000

    vm1: Disable openvpn.

--- hosts/vm1/configuration.nix
+++ hosts/vm1/configuration.nix
@@ -161,14 +161,6 @@ in
         enable = true;
         nssmdns4 = true;
       };
-      openvpn = {
-        servers.surfshark = {
-          config = ''
-            config /etc/openvpn/uk-man.prod.surfshark.com_tcp.ovpn
-            auth-user-pass /etc/openvpn/surfshark.pass
-          '';
-        };
-      };
     };
   };
 }
