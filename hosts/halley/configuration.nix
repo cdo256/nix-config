7d0c47ecea0db70ae83af85e03e152edaf58330d
Christina O'Donnell <cdo@mutix.org>
2024-12-20 02:17:54 +0000

    hosts: Restructure host modules to have config and options.

--- hosts/halley/configuration.nix
+++ hosts/halley/configuration.nix
@@ -4,16 +4,6 @@ let
   scriptsPackage = pkgs.callPackage ./scripts/default.nix {};
 in
 {
-  nixpkgs.config.allowUnfree = true;
-  nix = {
-    settings.experimental-features = [ "nix-command" "flakes" ];
-  };
-  systemd.services.nix-daemon = {
-    enable = true;
-    serviceConfig = {
-      EnvironmentFile = "-/etc/nix/nix-daemon-environment";
-    };
-  };
   imports =
     [
       ./hardware-configuration.nix
@@ -22,155 +12,168 @@ in
       ../../modules/borgbase.nix
       inputs.home-manager.nixosModules.default
     ];
+  
+  config = {
+    nixpkgs.config.allowUnfree = true;
+    nix = {
+      settings.experimental-features = [ "nix-command" "flakes" ];
+    };
+    systemd.services.nix-daemon = {
+      enable = true;
+      serviceConfig = {
+        EnvironmentFile = "-/etc/nix/nix-daemon-environment";
+      };
+    };
 
-  system.stateVersion = "24.05";
+    system.stateVersion = "24.05";
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/nvme0n1";
+    boot.loader.grub.useOSProber = true;
 
-  boot.initrd.luks.devices."luks-58655766-776f-42ca-96b1-a87a3e21508f".device = "/dev/disk/by-uuid/58655766-776f-42ca-96b1-a87a3e21508f";
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/boot/crypto_keyfile.bin" = null;
-  };
+    boot.initrd.luks.devices."luks-58655766-776f-42ca-96b1-a87a3e21508f".device = "/dev/disk/by-uuid/58655766-776f-42ca-96b1-a87a3e21508f";
+    # Setup keyfile
+    boot.initrd.secrets = {
+      "/boot/crypto_keyfile.bin" = null;
+    };
 
-  boot.loader.grub.enableCryptodisk = true;
+    boot.loader.grub.enableCryptodisk = true;
 
-  boot.initrd.luks.devices."luks-26f30444-e729-4254-808d-16e12eec659f".keyFile = "/boot/crypto_keyfile.bin";
-  boot.initrd.luks.devices."luks-58655766-776f-42ca-96b1-a87a3e21508f".keyFile = "/boot/crypto_keyfile.bin";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    boot.initrd.luks.devices."luks-26f30444-e729-4254-808d-16e12eec659f".keyFile = "/boot/crypto_keyfile.bin";
+    boot.initrd.luks.devices."luks-58655766-776f-42ca-96b1-a87a3e21508f".keyFile = "/boot/crypto_keyfile.bin";
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  boot.kernelModules = [ "v4l2loopback" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
-
-  networking.hostName = "halley";
-  networking.wireless.enable = false;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/London";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
-  console.keyMap = "uk";
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  security.polkit.enable = true;
-  security.sudo = {
-    enable =  true;
-  };
-
-  users.users.cdo = {
-    uid = 1000;
-    isNormalUser = true;
-    description = "Christina O'Donnell";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
+    boot.kernelModules = [ "v4l2loopback" ];
+    boot.extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
     ];
-    shell = pkgs.fish;
-  };
+    boot.extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
 
-  home-manager = {
-    backupFileExtension = ".nix.bak";
-    extraSpecialArgs = {
-      inherit inputs;
-      inherit files;
+    networking.hostName = "halley";
+    networking.wireless.enable = false;
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/London";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_GB.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
     };
-    users.cdo = import ../../home/client.nix;
-  };
 
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
-    config.common.default = [
-      "kde"
-    ];
-  };
+    console.keyMap = "uk";
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    security.polkit.enable = true;
+    security.sudo = {
+      enable =  true;
+    };
 
-  environment = {
-    systemPackages = [
-      pkgs.vim
-      pkgs.wget
-      pkgs.git
-      pkgs.ungoogled-chromium
-      #pkgs.guix
-      #scriptsPackage
-      pkgs.libimobiledevice
-      pkgs.ifuse
-      pkgs.usbmuxd
-      pkgs.kdePackages.dolphin
-      pkgs.kdePackages.qtwayland
-    ];
-  };
+    users.users.cdo = {
+      uid = 1000;
+      isNormalUser = true;
+      description = "Christina O'Donnell";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [
+      ];
+      shell = pkgs.fish;
+    };
 
-  #programs.mtr.enable = true;
-  programs = {
-    gnupg.agent = {
+    home-manager = {
+      backupFileExtension = ".nix.bak";
+      extraSpecialArgs = {
+        inherit inputs;
+        inherit files;
+      };
+      users.cdo = import ../../home/client.nix;
+    };
+
+    xdg.portal = {
       enable = true;
-      enableSSHSupport = true;
+      extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+      config.common.default = [
+        "kde"
+      ];
     };
-    sway.enable = true;
-    fish.enable = true;
-    firefox.enable = true;
-  };
 
-  services = {
-    logind = {
-      lidSwitch = "ignore";
-      powerKey = "suspend";
+    environment = {
+      systemPackages = [
+        pkgs.vim
+        pkgs.wget
+        pkgs.git
+        pkgs.ungoogled-chromium
+        #pkgs.guix
+        #scriptsPackage
+        pkgs.libimobiledevice
+        pkgs.ifuse
+        pkgs.usbmuxd
+        pkgs.kdePackages.dolphin
+        pkgs.kdePackages.qtwayland
+      ];
     };
-    xserver = {
-      enable = true;
-      displayManager.gdm.enable = true;
-      displayManager.gdm.wayland = true;
-      desktopManager.gnome.enable = true;
-      xkb.layout = "gb";
-      xkb.variant = "";
+
+    #programs.mtr.enable = true;
+    programs = {
+      gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
+      sway.enable = true;
+      fish.enable = true;
+      firefox.enable = true;
     };
-    libinput.enable = true;
-    openssh.enable = true;
-    printing.enable = true;
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
-    borgbase.enable = true;
-    borgbase.repository = config.devices.${config.networking.hostName}.resticRepo;
-    syncnet.enable = true;
-    syncnet.devices = config.devices;
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-    };
-    openvpn = {
-      servers.surfshark = {
-        config = ''
-          config /etc/openvpn/uk-man.prod.surfshark.com_tcp.ovpn
-          auth-user-pass /etc/openvpn/surfshark.pass
-        '';
+
+    services = {
+      logind = {
+        lidSwitch = "ignore";
+        powerKey = "suspend";
+      };
+      xserver = {
+        enable = true;
+        displayManager.gdm.enable = true;
+        displayManager.gdm.wayland = true;
+        desktopManager.gnome.enable = true;
+        xkb.layout = "gb";
+        xkb.variant = "";
+      };
+      libinput.enable = true;
+      openssh.enable = true;
+      printing.enable = true;
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
+      borgbase.enable = true;
+      borgbase.repository = config.devices.${config.networking.hostName}.resticRepo;
+      syncnet.enable = true;
+      syncnet.devices = config.devices;
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+      };
+      openvpn = {
+        servers.surfshark = {
+          config = ''
+            config /etc/openvpn/uk-man.prod.surfshark.com_tcp.ovpn
+            auth-user-pass /etc/openvpn/surfshark.pass
+          '';
+        };
       };
     };
   };
