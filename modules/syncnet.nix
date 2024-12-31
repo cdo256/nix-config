0a3c7e67b22d11288347d438424eb00c20dae218
Christina O'Donnell <cdo@mutix.org>
2024-12-31 00:25:07 +0000

    syncnet: Fix folder devices.

--- modules/syncnet.nix
+++ modules/syncnet.nix
@@ -18,26 +18,26 @@ in
       folders = {
         "sync" = {
           path = "/home/cdo/sync";
-          cfg.devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
+          devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
         };
         "org" = {
           path = "/home/cdo/sync/org";
-          cfg.devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
+          devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
         };
         "org-roam" = {
           path = "/home/cdo/sync/org-roam";
-          cfg.devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
+          devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
         };
         "secure" = {
           path = "/home/cdo/sync/secure";
-          cfg.devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
+          devices = cfg.devices.pcs ++ cfg.devices.androidDevices;
         };
       } // builtins.listToAttrs (map (androidDevice:
         {
           name = androidDevice.name + "-root";
           value = {
             path = "/home/cdo/sync/" + androidDevice.name;
-            cfg.devices = [ androidDevice ] ++ cfg.devices.pcs;
+            devices = [ androidDevice ] ++ cfg.devices.pcs;
           };
         }) cfg.devices.androidDevices);
     in
@@ -65,8 +65,9 @@ in
           ) (builtins.filter (device: device ? "syncthingId") cfg.devices.allDevices));
           folders = builtins.mapAttrs (name: folder:
             {
+              enable = true;
               path = folder.path;
-              cfg.devices = (map (device: device.name) cfg.devices.allDevices);
+              devices = (map (device: device.name) folder.devices);
               versioning = {
                 type = "staggered";
                 params.maxAge = "365";
