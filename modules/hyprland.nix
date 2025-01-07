f99fdb82e809bc67d234a3ef1430119aeea86927
Christina O'Donnell <cdo@mutix.org>
2025-01-07 19:00:40 +0000

    hyprland: Add reccomended options from vimjoyer.

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -13,11 +13,18 @@ in
     enable = true;
     package = inputs.hyprland.packages.${system}.hyprland;
     portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
+    xwayland.enable = true;
   };
-  environment.systemPackages = [
-    pkgs.kitty # Default terminal
-  ];
+  environment = {
+    systemPackages = [
+      pkgs.kitty # Default terminal
+    ];
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+  };
   hardware.opengl = {
+    enable = true;
     package = hyprland-pkgs.mesa.drivers;
     driSupport32Bit = true;
     package32 = hyprland-pkgs.pkgsi686Linux.mesa.drivers;
