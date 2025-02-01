917dc38ff4fccc8a4553d3f4aeb1ff03bc654ea0
Christina O'Donnell <cdo@mutix.org>
2025-02-01 16:31:01 +0000

    halley: Set hyprland scaling.

--- systems/halley/nixos.nix
+++ systems/halley/nixos.nix
@@ -12,16 +12,14 @@
     #  ../../os-modules/syncnet.nix
     #  ../../os-modules/borgbase.nix
     #  ../../os-modules/sops.nix
-    #  ../../os-modules/hyprland.nix
-    #  ../../os-modules/fonts.nix
     #  inputs.sops-nix.nixosModules.sops
   ];
 
   config = {
+    home-manager.users.${config.args.owner}.wayland.windowManager.hyprland.settings.monitor = [
+      "eDP-1, 1920x1080, 0x0, 0.5"
+    ];
     services = {
-      xserver = {
-        xkb.model = "";
-      };
       printing.enable = true;
       #borgbase.enable = true;on wayland, should I configure libinput exclusively or will I also need to configure xkb (for cases where I'm using an x-wayland applicaiton?
       #syncnet.enable = true;
