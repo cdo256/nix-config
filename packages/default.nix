d03bafbc10ea38eeeb020b059f1a9947f3194d91
Christina O'Donnell <cdo@mutix.org>
2025-02-03 17:30:19 +0000

    Add package sc-im.

--- packages/default.nix
+++ packages/default.nix
@@ -11,6 +11,7 @@
         home-manager = inputs.home-manager.defaultPackage.x86_64-linux;
         nixvim = inputs.nixvim.packages.x86_64-linux.default;
         files = pkgs.callPackage ./files.nix { };
+        sc-im = pkgs.sc-im.overrideAttrs { xlsSupport = true; };
       };
     };
 }
