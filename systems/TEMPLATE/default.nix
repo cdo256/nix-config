d86e33ede0cbddfe7838152a753799ea7df06e95
Christina O'Donnell <cdo@mutix.org>
2025-02-22 15:10:10 +0000

    Add TEMPLATE system.

---
+++ systems/TEMPLATE/default.nix
@@ -0,0 +1,67 @@
+{
+  self,
+  inputs,
+  config,
+  ...
+}:
+{
+  flake.systems.halley = {
+    type = "TODO"; # "laptop", "desktop", "phone", "tablet", "server"
+    os = "TODO"; # "nixos", "debian", "android"
+    hostname = "TODO";
+    syncthingId = "XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX";
+    sshPublicKey = "ssh-ed25519 XXXXXXXXXXXXXXXXXXXXXXXXXXXX cdo@HOSTNAME";
+    #arch = "x86_64-linux";
+    owner = "cdo";
+    packages =
+      let
+        root = config.flake.repoRoot + "/manifests";
+      in
+      {
+        system = [
+          (root + "/base.nix")
+          (root + "/system.nix")
+          (root + "/terminal.nix")
+        ];
+        home = [
+          (root + "/base.nix")
+          (root + "/desktop.nix")
+          (root + "/development.nix")
+          (root + "/terminal.nix")
+          (root + "/sysadmin.nix")
+        ];
+      };
+    modules =
+      let
+        root = config.flake.repoRoot + "/modules/nixos";
+      in
+      [
+        ./nixos.nix
+        ./hardware.nix
+        ./boot.nix
+        inputs.home-manager.nixosModules.home-manager
+        (root + "/base.nix")
+        (root + "/cdo.nix")
+        (root + "/superuser.nix")
+        (root + "/system-packages.nix")
+        (root + "/laptop.nix")
+        (root + "/hm.nix")
+        (root + "/fonts.nix")
+        (root + "/locale.nix")
+        (root + "/nix.nix")
+        (root + "/security.nix")
+        (root + "/networking.nix")
+        (root + "/shell.nix")
+        (root + "/xserver.nix")
+        (root + "/graphical.nix")
+        (root + "/hyprland.nix")
+        (root + "/video.nix")
+        (root + "/vpn.nix")
+        (root + "/unfree.nix")
+      ];
+    args = {
+      flake = self;
+      inherit (config.flake) repoRoot;
+    };
+  };
+}
