bb6048678f7ed833a2007abdbb21edddf0b4c86f
Christina O'Donnell <cdo@mutix.org>
2025-04-19 11:32:29 +0100

    home.kbLayout: Add option.

--- modules/home/cdo/vars.nix
+++ modules/home/cdo/vars.nix
@@ -16,6 +16,43 @@ in
     defaults = mkOption {
       type = types.attrs;
     };
+    # From https://github.com/NixOS/nixpkgs/blob/7650c61104186974b24d9ba97f6a2ea018581f09/nixos/modules/services/x11/xserver.nix
+    kbLayout = {
+      layout = mkOption {
+        type = types.str;
+        default = "us";
+        description = ''
+          X keyboard layout, or multiple keyboard layouts separated by commas.
+        '';
+      };
+
+      model = mkOption {
+        type = types.str;
+        default = "pc104";
+        example = "presario";
+        description = ''
+          X keyboard model.
+        '';
+      };
+
+      options = mkOption {
+        type = types.commas;
+        default = "terminate:ctrl_alt_bksp";
+        example = "grp:caps_toggle,grp_led:scroll";
+        description = ''
+          X keyboard options; layout switching goes here.
+        '';
+      };
+
+      variant = mkOption {
+        type = types.str;
+        default = "";
+        example = "colemak";
+        description = ''
+          X keyboard variant.
+        '';
+      };
+    };
   };
   config = {
     home = {
@@ -35,5 +72,11 @@ in
         BROWSER = config.home.defaults.browser;
         TERMINAL = config.home.defaults.terminal;
       };
+      kbLayout = {
+        layout = "gb";
+        variant = "";
+        options = "caps:escape";
+      };
+    };
   };
 }
