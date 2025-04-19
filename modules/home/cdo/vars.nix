8a5c23ba43796025ce9be6b0010448938d0cbb5b
Christina O'Donnell <cdo@mutix.org>
2025-04-19 11:29:16 +0100

    home.defaults: Add options.

--- modules/home/cdo/vars.nix
+++ modules/home/cdo/vars.nix
@@ -2,13 +2,38 @@
   inputs,
   pkgs,
   args,
+  config,
   ...
 }:
+let
+  inherit (inputs.nixpkgs.lib)
+    mkOption
+    types
+    ;
+in
 {
-  home.username = "cdo";
-  home.sessionVariables = {
-    EDITOR = "${inputs.nixvim.packages.${args.arch}.default}/bin/nvim";
-    BROWSER = "${pkgs.brave}/bin/brave";
-    TERMINAL = "${pkgs.kitty}/bin/kitty";
+  options.home = {
+    defaults = mkOption {
+      type = types.attrs;
+    };
+  };
+  config = {
+    home = {
+      username = "cdo";
+      defaults = {
+        editor = "${inputs.nixvim.packages.${args.arch}.default}/bin/nvim";
+        browser = "${pkgs.brave}/bin/brave";
+        terminal = "${pkgs.kitty}/bin/kitty";
+        desktop = "sway";
+        launcher = "${pkgs.wofi}/bin/wofi";
+        fileManager = "${pkgs.kdePackages.dolphin}/bin/dolphin";
+        emailProgram = "${pkgs.thunderbird}/bin/thunderbird";
+        passwordManager = "${pkgs.keepassxc}/bin/keepassxc";
+      };
+      sessionVariables = {
+        EDITOR = config.home.defaults.editor;
+        BROWSER = config.home.defaults.browser;
+        TERMINAL = config.home.defaults.terminal;
+      };
   };
 }
