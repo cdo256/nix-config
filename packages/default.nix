ba21727e4261b83253a8c9980f83ac2b11f65c2c
Christina O'Donnell <cdo@mutix.org>
2025-04-23 12:17:32 +0100

    packages: Correct home-manager package attr path.

--- packages/default.nix
+++ packages/default.nix
@@ -8,7 +8,7 @@
     { system, pkgs, ... }:
     {
       packages = {
-        home-manager = inputs.home-manager.defaultPackage.x86_64-linux;
+        home-manager = inputs.home-manager.packages.x86_64-linux.default;
         nixvim = inputs.nixvim.packages.x86_64-linux.default;
         files = pkgs.callPackage ./files.nix { };
         sc-im = pkgs.sc-im.overrideAttrs { xlsSupport = true; };
