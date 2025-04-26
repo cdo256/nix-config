fce3dcc8df7a6fc043793fbb1b2932cb2e97c43f
Christina O'Donnell <cdo@mutix.org>
2025-04-26 17:11:21 +0100

    modules/home/cdo: Remove slashes.

--- modules/home/cdo/default.nix
+++ modules/home/cdo/default.nix
@@ -9,14 +9,14 @@ let
   inherit (flake.lib) withDefaultPath;
   baseModules = [
     ./vars.nix
-    "/base.nix"
-    "/fish.nix"
-    "/fs.nix"
-    "/git.nix"
-    "/hyprland.nix"
-    "/hyprpanel.nix"
-    "/packages.nix"
-    "/sway.nix"
+    "base.nix"
+    "fish.nix"
+    "fs.nix"
+    "git.nix"
+    "hyprland.nix"
+    "hyprpanel.nix"
+    "packages.nix"
+    "sway.nix"
   ];
   modules = map (withDefaultPath "/modules/home") (args.modules.home ++ baseModules);
 in
