c4851a9822c3d1b3b90d064e6663ba58729f0101
Christina O'Donnell <cdo@mutix.org>
2025-05-08 12:19:14 +0100

    halley: Enable subpixel font rendering.

--- systems/halley/nixos.nix
+++ systems/halley/nixos.nix
@@ -2,4 +2,7 @@
   services.printing.enable = true;
   services.syncnet.enable = true;
   services.borgbase.enable = true;
+
+  fonts.fontconfig.subpixel.rgba = "rgb";
+  fonts.fontconfig.subpixel.lcdfilter = "default";
 }
