477df446a7df9061e17e830120db86fc6da18038
Christina O'Donnell <cdo@mutix.org>
2025-05-08 18:54:48 +0100

    peter: font rendering options.

--- systems/peter/nixos.nix
+++ systems/peter/nixos.nix
@@ -12,5 +12,9 @@
       syncnet.enable = true;
       borgbase.enable = true;
     };
+    fonts.fontconfig.subpixel.rgba = "rgb";
+    fonts.fontconfig.subpixel.lcdfilter = "default";
+    hardware.video.hidpi.enable = true;
+    fonts.optimizeForVeryHighDPI = true;
   };
 }
