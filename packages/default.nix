e492fe6719f720dcda97a8da77aec2ace61dc3b8
Christina O'Donnell <cdo@mutix.org>
2025-05-15 17:25:33 +0100

    packages: python-utils: New empty package.

--- packages/default.nix
+++ packages/default.nix
@@ -11,6 +11,7 @@
         home-manager = inputs.home-manager.packages.x86_64-linux.default;
         nixvim = inputs.nixvim.packages.x86_64-linux.default;
         files = pkgs.callPackage ./files.nix { };
+        python-utils = pkgs.callPackage ./python-utils { };
         sc-im = pkgs.sc-im.overrideAttrs { xlsSupport = true; };
       };
     };
