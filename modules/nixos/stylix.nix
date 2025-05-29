6350b77d35b0f9d5db15cf2d8acd99a7beb9b061
Christina O'Donnell <cdo@mutix.org>
2025-05-29 23:21:14 +0100

    nixos: stylix: Tweak colors.

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -4,22 +4,23 @@
     enable = true;
     polarity = "dark";
     base16Scheme = {
-      base00 = "#000005";
-      base01 = "#1f3d48";
+      base00 = "#000000";
+      base01 = "#0f0d38";
       base02 = "#c5020e";
-      base03 = "#36a3a3";
-      base04 = "#6d2f8c";
-      base05 = "#99d3e6";
-      base06 = "#eebdcd";
-      base07 = "#c0caf5";
+      base03 = "#16e3a3";
+      base04 = "#9d3fbc";
+      base05 = "#09d3e6";
+      base06 = "#dead3d";
+      base07 = "#f0ca4f";
       base08 = "#ff6e89";
       base09 = "#ff9e64";
-      base0A = "#ffb549";
+      base0A = "#ffca3f";
       base0B = "#a0ff39";
       base0C = "#00d9ff";
       base0D = "#6f9fff";
       base0E = "#9255ff";
-      base0F = "#90bbff";
+      base0F = "#40ebff";
     };
+    targets.chromium.enable = true;
   };
 }
