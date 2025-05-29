ec5bf89bdb1d1e7d5d74fd76d2c1fa43c984f7b4
Christina O'Donnell <cdo@mutix.org>
2025-05-29 14:01:03 +0100

    nixos: base: Use flake.lib.types definition.

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -1,4 +1,5 @@
 {
+  flake,
   config,
   options,
   inputs,
@@ -8,6 +9,9 @@ let
   inherit (inputs.nixpkgs.lib)
     mkOption
     types
+    ;
+  inherit (flake.lib.types)
+    system
     ;
   relpathType = types.oneOf [
     types.str
@@ -20,53 +24,9 @@ let
   ];
 in
 {
-  options.args = {
-    arch = mkOption {
-      type = types.str;
-      description = "Architecture string eg x86_64-linux";
-    };
-    type = mkOption {
-      # TODO: Should be from a list of strings.
-      type = types.str;
-      description = "server, laptop, destop, vm, etc.";
-    };
-    owner = mkOption {
-      type = types.str;
-      description = "String of username of person who owns this system.";
-    };
-    hostname = mkOption {
-      type = types.str;
-      description = "The name of the machine.";
-    };
-    graphical = mkOption {
-      type = types.bool;
-      default = config.args.type == "laptop" || config.args.type == "desktop";
-      description = "Does this machine have a graphical display output.";
-    };
-    packages = {
-      home = mkOption {
-        type = types.listOf relpathType;
-        default = [ ];
-        description = "List of manifests packages to run on the owner's home-manager configuration.";
-      };
-      system = mkOption {
-        type = types.listOf relpathType;
-        default = [ ];
-        description = "List of system manifests to run on this system.";
-      };
-    };
-    modules = {
-      home = mkOption {
-        type = types.listOf moduleType;
-        default = [ ];
-        description = "List of home-manager modules.";
-      };
-      nixos = mkOption {
-        type = types.listOf moduleType;
-        default = [ ];
-        description = "List of nixos modules.";
-      };
-    };
+  options.args = mkOption {
+    type = system;
+    description = "Description of a NixOS system";
   };
   config = {
     system.stateVersion = "24.05";
