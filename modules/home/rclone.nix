23cf871baf82cb0fd07dc35b31d51984c61a7c3e
Christina O'Donnell <cdo@mutix.org>
2026-02-01 10:17:09 +0000

    home: rclone: bisyncs->mounts.

--- modules/home/rclone.nix
+++ modules/home/rclone.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   programs.rclone = {
-    enable = false; # Currently broken
+    enable = true; # Currently broken
     remotes.g = {
       config = {
         type = "drive";
@@ -12,37 +12,35 @@
         client_id = config.sops.secrets."google-drive/client_id".path;
         client_secret = config.sops.secrets."google-drive/client_secret".path;
       };
-      bisyncs = {
+      mounts = {
         "obsidian" = {
           enable = true;
-          localPath = "${config.home.homeDirectory}/obsidian";
+          mountPoint = "${config.home.homeDirectory}/obsidian";
           options = {
             umask = "077";
           };
         };
-        "sync" = {
-          enable = true;
-          localPath = "${config.home.homeDirectory}/sync";
-          options = {
-            umask = "077";
-          };
-        };
-        "secure" = {
-          enable = true;
-          localPath = "${config.home.homeDirectory}/.local/sync/secure";
-          options = {
-            umask = "077";
-          };
-        };
-        "config" = {
-          enable = true;
-          localPath = "${config.home.homeDirectory}/.local/sync/config";
-          options = {
-            umask = "077";
-          };
-        };
-      };
-      mounts = {
+        #"sync" = {
+        #  enable = true;
+        #  mountPoint = "${config.home.homeDirectory}/sync";
+        #  options = {
+        #    umask = "077";
+        #  };
+        #};
+        #"secure" = {
+        #  enable = true;
+        #  localPath = "${config.home.homeDirectory}/.local/sync/secure";
+        #  options = {
+        #    umask = "077";
+        #  };
+        #};
+        #"config" = {
+        #  enable = true;
+        #  localPath = "${config.home.homeDirectory}/.local/sync/config";
+        #  options = {
+        #    umask = "077";
+        #  };
+        #};
         "archive" = {
           enable = true;
           mountPoint = "${config.home.homeDirectory}/archive";
